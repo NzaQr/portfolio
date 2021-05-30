dffe03560ba0f150a8b62227195ceef6794a628e
Nazareno Quiroga <naz.quiroga@gmail.com>
2021-05-29 22:23:25 -0300

    Add skills section and rearrange sections

--- src/App.js
+++ src/App.js
@@ -2,10 +2,32 @@ import "./App.css";
 import Card from "./components/Card";
 import photo from "./assets/photo.jpg";
 import Projects from "./components/Projects";
+import Header from "./components/Header";
+import { AiOutlineHtml5 } from "react-icons/ai";
+import { DiCss3, DiReact } from "react-icons/di";
+import { IoLogoJavascript, IoLogoFirebase } from "react-icons/io5";
+import { FaGitAlt } from "react-icons/fa";
+import { useRef } from "react";
 
 function App() {
+  const aboutScroll = useRef();
+  const projectsScroll = useRef();
+
+  const handleClickAbout = () => {
+    aboutScroll.current.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const handleClickProjects = () => {
+    projectsScroll.current.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <>
+      {/* CHANGE NAV MENU STYLE */}
+      <Header
+        handleClickAbout={handleClickAbout}
+        handleClickProjects={handleClickProjects}
+      />
       <Card />
       <section className="about">
         <div className="about-container">
@@ -13,6 +35,7 @@ function App() {
             className="photo"
             src={photo}
             alt="persona(Nazareno) con un rio de fondo"
+            ref={aboutScroll}
           />
           <h1>Soy un desarrollador autodidacta de Buenos Aires 🇦🇷</h1>
         </div>
@@ -22,16 +45,7 @@ function App() {
           IT.
         </p>
       </section>
-      <section className="projects">
-        <h1>Proyectos</h1>
-        <div className="projects-component">
-          <Projects />
-        </div>
-        <p className="projects-invite">
-          Te invito a ver más en mi{" "}
-          <a href="https://github.com/NzaQr">GitHub</a>!
-        </p>
-      </section>
+
       <section className="experience">
         <div className="timeline-container">
           <div className="circle"></div>
@@ -45,11 +59,50 @@ function App() {
           seguir aprendiendo.
         </p>
         <p>
-          En 2020 hice un bootcamp de JavaScript y un curso completo de Frontend
-          en el que profundicé conceptos y aprendí React, ambos hechos en{" "}
+          En 2020 hice un bootcamp de JavaScript y un curso completo en el que
+          profundicé conceptos y aprendí React, ambos hechos en{" "}
           <a href="https://scrimba.com/">Scrimba</a>.
         </p>
         <p>Actualmente busco empleo como Frontend Developer.</p>
+      </section>
+      <section ref={projectsScroll} className="projects">
+        <h1>Proyectos</h1>
+        <div className="projects-component">
+          <Projects />
+        </div>
+        <p className="projects-invite">
+          Te invito a ver más en mi{" "}
+          <a href="https://github.com/NzaQr">GitHub</a>!
+        </p>
+      </section>
+      <section className="skills">
+        <h1>Tecnologías</h1>
+        <ul>
+          <li>
+            <AiOutlineHtml5 className="icon" />
+            <p>HTML5</p>
+          </li>
+          <li>
+            <DiCss3 className="icon" />
+            <p>CSS3</p>
+          </li>
+          <li>
+            <IoLogoJavascript className="icon" />
+            <p>JavaScript</p>
+          </li>
+          <li>
+            <DiReact className="icon" />
+            <p>React</p>
+          </li>
+          <li>
+            <IoLogoFirebase className="icon" />
+            <p>Firebase</p>
+          </li>
+          <li>
+            <FaGitAlt className="icon" />
+            <p>Git</p>
+          </li>
+        </ul>
       </section>
       <section className="contact">
         <p>
